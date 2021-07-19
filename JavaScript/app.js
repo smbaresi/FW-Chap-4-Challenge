e420a056f912b1d69ebe9633f9e02df382e3293b
Baresi <sumardiyanto.baresi@gmail.com>
2021-07-19 19:03:57 +0700

    add score, reload and greybox in js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,35 @@
-const vs_div = document.getElementById("vsAction");
-const refresh_div = document.getElementById("refreshAction");
-const result_div = document.querySelector(".result");
-const playerGreyBox_div = document.querySelector(".greyBox-js.playerGrey")
-const comGreyBox_div = document.querySelector(".greyBox-js.comGrey")
-const batu_div = document.getElementById("batu");
-const kertas_div = document.getElementById("kertas");
-const gunting_div = document.getElementById("gunting");
-const comBatu_div = document.getElementById("comBatu");
-const comKertas_div = document.getElementById("comKertas");
-const comGunting_div = document.getElementById("comGunting");
+let isReady = true;
+let playerScore = 0;
+let comScore = 0;
+
+const [
+    playerScore_span,
+    comScore_span,
+    vs_div,
+    refresh_div,
+    playerGreyBox_div,
+    comGreyBox_div,
+    batu_div,
+    kertas_div, 
+    gunting_div,
+    batu2_div,
+    kertas2_div, 
+    gunting2_div,
+    ] = [
+    document.getElementById("playerScore"),
+    document.getElementById("comScore"),
+    document.getElementById("vsAction"),
+    document.getElementById("refreshAction"),
+    document.querySelector(".greyBox-js.playerGrey"),
+    document.querySelector(".greyBox-js.comGrey"),
+    document.getElementById("batu"),
+    document.getElementById("kertas"),
+    document.getElementById("gunting"),
+    document.getElementById("batu2"),
+    document.getElementById("kertas2"),
+    document.getElementById("gunting2"),
+]
+
 
 function getComputerChoice() {
     const choices = ["batu", "kertas", "gunting"]
@@ -19,50 +40,87 @@ function getComputerChoice() {
 function main() {
     batu_div.addEventListener('click', function () {
         game("batu")
+        isReady = false
     })
 
     kertas_div.addEventListener('click', function () {
         game("kertas")
+        isReady = false
     })
 
     gunting_div.addEventListener('click', function () {
         game("gunting")
+        isReady = false
     })
+    refresh_div.addEventListener('click', function (){
+        reload()
+        isReady = true
+    })
+    
 }
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice) {
-        case "batugunting":
-        case "kertasbatu":
-        case "guntingkertas":
-            win();
-            break;
-        case "batukertas":
-        case "kertasgunting":
-        case "guntingbatu":
-            lose();
-            break;
-        case "batubatu":
-        case "kertaskertas":
-        case "guntinggunting":
-            draw();
-            break;
+    if(isReady) {
+        switch (userChoice + computerChoice) {
+            case "batugunting":
+            case "kertasbatu":
+            case "guntingkertas":
+                win(userChoice, computerChoice);
+                break;
+            case "batukertas":
+            case "kertasgunting":
+            case "guntingbatu":
+                lose(userChoice, computerChoice);
+                break;
+            case "batubatu":
+            case "kertaskertas":
+            case "guntinggunting":
+                draw(userChoice, computerChoice);
+                break;
+        }    
+    }
+    else {
+        alert("Reload dl coi")
     }
 }
 
-function win() {
+function win(userChoice, computerChoice) {
     vs_div.innerHTML = "PLAYER 1 WIN".fontsize(38);
+    vs_div.classList.add("winlosebackground-js");
+    playerScore++;
+    playerScore_span.innerHTML = playerScore;
+    comScore_span.innerHTML = comScore;
+    document.getElementById(userChoice).classList.add("greyBox-js");
+    document.getElementById(computerChoice+"2").classList.add("greyBox-js");
 }
 
-function lose() {
+function lose(userChoice, computerChoice) {
     vs_div.innerHTML = "COM WIN".fontsize(38);
+    vs_div.classList.add("winlosebackground-js");
+    comScore++;
+    playerScore_span.innerHTML = playerScore;
+    comScore_span.innerHTML = comScore;
+    document.getElementById(userChoice).classList.add("greyBox-js");
+    document.getElementById(computerChoice+"2").classList.add("greyBox-js");
 
 }
 
-function draw() {
+function draw(userChoice, computerChoice) {
     vs_div.innerHTML = "DRAW".fontsize(38);
+    vs_div.classList.add("winlosebackground-js");
+    document.getElementById(userChoice).classList.add("greyBox-js");
+    document.getElementById(computerChoice+"2").classList.add("greyBox-js");
+}
 
+function reload() {
+    batu_div.classList.remove("greyBox-js");
+    kertas_div.classList.remove("greyBox-js");
+    gunting_div.classList.remove("greyBox-js");
+    batu2_div.classList.remove("greyBox-js");
+    kertas2_div.classList.remove("greyBox-js");
+    gunting2_div.classList.remove("greyBox-js");
+    // location.reload()
 }
 
 main()
